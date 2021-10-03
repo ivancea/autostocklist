53ab6b44ca69d8bedd9cb515332110c71067c1e3
Iván Cea Fontenla <ivancea96@outlook.com>
2021-10-03 22:52:00 +0200

    Fixed procedures not allowing to reduce losses or resupplies

--- database/scripts/10_create_stock_procedures.sql
+++ database/scripts/10_create_stock_procedures.sql
@@ -1,39 +1,67 @@
-CREATE PROCEDURE stock.update_stock_loss(
-	item_id INTEGER,
-	date DATE,
-	quantity INTEGER
+CREATE OR REPLACE PROCEDURE stock.update_stock_loss(
+	_item_id INTEGER,
+	_date DATE,
+	_quantity INTEGER
 )
-LANGUAGE SQL
+LANGUAGE plpgsql
 AS $$
+DECLARE
+BEGIN
 	-- Update item stock
 	UPDATE stock.item
-	SET stock = stock - quantity
-	WHERE id = item_id;
+	SET stock = stock - _quantity
+	WHERE id = _item_id;
 
-	-- Add/Update stock loss historic
-	INSERT INTO stock.loss (item_id, date, quantity)
-    VALUES($1, $2, $3)
-    ON CONFLICT (item_id, date)
-    DO
-    UPDATE SET quantity = stock.loss.quantity + excluded.quantity;
-$$;
+	IF _quantity < 0 THEN
+		-- Update existing stock loss
+		UPDATE stock.loss
+		SET quantity = quantity + _quantity
+		WHERE item_id = _item_id
+			AND date = _date;
+		
+		IF NOT FOUND THEN
+		     RAISE 'No loss entries for (%, %)', _item_id, _date;
+		END IF;
+	ELSE
+		-- Add/Update stock loss
+		INSERT INTO stock.loss (item_id, date, quantity)
+	    VALUES(_item_id, _date, _quantity)
+	    ON CONFLICT (item_id, date)
+	    DO
+	    UPDATE SET quantity = stock.loss.quantity + excluded.quantity;
+	END IF;
+END $$;
 
-CREATE PROCEDURE stock.update_stock_resupply(
-	item_id INTEGER,
-	date DATE,
-	quantity INTEGER
+CREATE OR REPLACE PROCEDURE stock.update_stock_resupply(
+	_item_id INTEGER,
+	_date DATE,
+	_quantity INTEGER
 )
-LANGUAGE SQL
+LANGUAGE plpgsql
 AS $$
+DECLARE
+BEGIN
 	-- Update item stock
 	UPDATE stock.item
-	SET stock = stock + quantity
-	WHERE id = item_id;
+	SET stock = stock + _quantity
+	WHERE id = _item_id;
 
-	-- Add/Update stock resupply historic
-	INSERT INTO stock.resupply (item_id, date, quantity)
-    VALUES($1, $2, $3)
-    ON CONFLICT (item_id, date)
-    DO
-    UPDATE SET quantity = stock.resupply.quantity + excluded.quantity;
-$$;
+	IF _quantity < 0 THEN
+		-- Update existing stock resupply
+		UPDATE stock.resupply
+		SET quantity = quantity + _quantity
+		WHERE item_id = _item_id
+			AND date = _date;
+		
+		IF NOT FOUND THEN
+		     RAISE 'No loss resupply for (%, %)', _item_id, _date;
+		END IF;
+	ELSE
+		-- Add/Update stock resupply
+		INSERT INTO stock.resupply (item_id, date, quantity)
+	    VALUES(_item_id, _date, _quantity)
+	    ON CONFLICT (item_id, date)
+	    DO UPDATE
+		SET quantity = stock.resupply.quantity + excluded.quantity;
+	END IF;
+END $$;
